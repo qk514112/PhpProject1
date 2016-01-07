cec4f77dcc4c6c131e50851c08637e5c37bce2c8
Admin <Admin@QL>
2016-01-08 00:30:54 +0800

    1, connect to mysql;

--- index.php
+++ index.php
@@ -11,12 +11,24 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        /* get images from database;
+        /** get images from database;
          *     connect database server;
          *         if connected;
          *             get images;
          *     close database;   
-         * 
+         **/
+        //Database
+        $mysql_host = '127.0.0.1';
+        $mysql_username = 'root';
+        $mysql_password = '123456';
+        $mysql_database = 'test';
+        $result = mysql_connect($mysql_host, $mysql_username, $mysql_password);
+        if (!$result) {
+            die(__LINE__ . ' Invalid connect: ' . mysql_error());
+        }
+        echo(mysql_error());
+        echo("aaaaa");
+         /** 
          * display images on screen;
          * scroll images automately;
          */
