b0ac2e691fe8dc59a6362b0c146fff95fba78f3c
Admin <Admin@QL>
2016-01-14 00:01:08 +0800

    1, connect to mysql; 2, before pdo connect commit the file;

--- index.php
+++ index.php
@@ -22,12 +22,19 @@ and open the template in the editor.
         $mysql_username = 'root';
         $mysql_password = '123456';
         $mysql_database = 'test';
-        $result = mysql_connect($mysql_host, $mysql_username, $mysql_password);
-        if (!$result) {
-            die(__LINE__ . ' Invalid connect: ' . mysql_error());
-        }
-        echo(mysql_error());
-        echo("aaaaa");
+//        $result = mysql_connect($mysql_host, $mysql_username, $mysql_password);
+//        if (!$result) {
+//            die(__LINE__ . ' Invalid connect: ' . mysql_error());
+//        }
+//        mysql_select_db($mysql_database) or die("Unable to select database.");
+//        $img_array = mysql_query("select url from t_img");
+            
+//        $connection = new mysqli($mysql_host, $mysql_username, $mysql_password, $mysql_database);
+        
+//        if ($connection->connect_error) {
+//            die("Connection failed:".$connection->connect_error);
+//        }
+        
          /** 
          * display images on screen;
          * scroll images automately;
